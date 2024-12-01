0ece5d65fa8661c57fb8ef8f4af6d4f5c45b61a2
vfe01 <villeelings@gmail.com>
2024-12-01 16:42:53 +0100

    Day 1 part 2

--- 01/main.py
+++ 01/main.py
@@ -25,6 +25,7 @@ def parse_input(file_name):
 
 input_file_names = ["input", "input_test"]
 
+
 for file_name in input_file_names:
     print("Input file name: ", file_name)
     left_list, right_list, list_length = parse_input(file_name)
@@ -32,10 +33,31 @@ for file_name in input_file_names:
     left_list.sort()
     right_list.sort()
 
-    sum = 0
+    part_one_sum = 0
     for i in range(list_length):
-        sum += abs(left_list[i] - right_list[i])
+        part_one_sum += abs(left_list[i] - right_list[i])
 
-    print(f"Answer: {sum}\n\n")
+    print(f"Part one answer: {part_one_sum}")
+
+
+    #Part 2
+    number_occurances = {}
+
+    for number in right_list:
+        number_string = str(number)
+        if number_string in number_occurances:
+            number_occurances[number_string] += 1
+        else:
+            number_occurances[number_string] = 1
     
+    part_two_sum = 0
+    for number in left_list:
+        number_string = str(number)
+        if number_string in number_occurances:
+            occurances_in_right_list = number_occurances[str(number)]
+        else:
+            occurances_in_right_list = 0
+        part_two_sum += number * occurances_in_right_list
     
+    print(f"Part two answer: {part_two_sum}")
+    print("\n")
