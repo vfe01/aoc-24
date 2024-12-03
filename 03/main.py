c5243cf9c11c90b03719e45aa98f18b2a0299511
vfe01 <villeelings@gmail.com>
2024-12-03 15:12:44 +0100

    day 3 part 2

--- 03/main.py
+++ 03/main.py
@@ -1,5 +1,5 @@
 import re
-from  typing import List
+from  typing import List, Pattern, Tuple
 
 class Multiplication:
     def __init__(self, multiplication_string:str):
@@ -10,28 +10,67 @@ class Multiplication:
     def product(self):
         return self._left_factor * self._right_factor
 
+class Do:
+    def __init__(self, do_string):
+        if do_string != "do()":
+            raise Exception("knas bror")
+        
+class Dont:
+    def __init__(self, do_string):
+        if do_string != "don't()":
+            raise Exception("knas bror")
+        
+def match_and_find_positions(text, pattern:Pattern[str], constructor):
+    matches = []
+    for match in pattern.finditer(text):
+        start, _ = match.span()
+        matches.append((constructor(match.group()), start))
+
+    return matches
+
 def parse_input(file_name) -> List[Multiplication]:
     with open(file_name, 'r') as file:
         content = file.read()
-    mul_pattern = re.compile(r"mul\(\d+,\d+\)")
-    matches = mul_pattern.findall(content)
-    multiplications = []
-    for match in matches:
-        multiplications.append(Multiplication(match))
+    
+    pattern_constructor_pairs:List[Tuple[Pattern[str],  object]] = [
+        (re.compile(r"mul\(\d+,\d+\)"), Multiplication),
+        (re.compile(r"do\(\)"), Do),
+        (re.compile(r"don't\(\)"), Dont)
+    ]
+    all_matches = []
+    for pattern, constructor in pattern_constructor_pairs:
 
-    return multiplications
+        matches = match_and_find_positions(content, pattern, constructor)
+        all_matches += matches
+    
+    
+    return sorted(all_matches, key=lambda x: x[1])
+
+def evaluate_memory(commands, conditionals_allowed):
+    dont_previously = False
+
+    if not conditionals_allowed:
+        commands = [command for command in commands if isinstance(command[0], Multiplication)]
+    sum = 0
+    for command in commands:
+        command_type = command[0]
+        if isinstance(command_type, Multiplication) and not dont_previously:
+            sum += command[0].product()
+        elif isinstance(command_type, Do) and dont_previously:
+            dont_previously = False
+        elif isinstance(command_type, Dont) and not dont_previously:
+            dont_previously = True
+    
+    return sum
+
+
 file_names = ['test_input', 'input']
 
 for file_name in file_names:
     print('File name: ',file_name)
     
-    multiplications: List[Multiplication] = parse_input(file_name)
+    matches: List[Multiplication] = parse_input(file_name)
+    
+    print('Part 1 solution: ', evaluate_memory(matches, conditionals_allowed=False)) #Test: 161, actual: 161085926 (correct)
 
-    sum = 0
-    for multi in multiplications:
-        sum += multi.product()
-    part_1_solution = sum
-    print('Part 1 solution: ', part_1_solution) #Test: 161, actual: 161085926 (correct)
-
-    part_2_solution = 69
-    print('Part 2 solution: ', part_2_solution) 
+    print('Part 2 solution: ', evaluate_memory(matches, conditionals_allowed=True))  #Test: 48, actual: 82045421 (correct)
